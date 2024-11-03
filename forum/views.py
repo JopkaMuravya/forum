5541144ecc417e5b78a6d49bf5884d65b1ef428c
Nania01 <148682556+Nania01@users.noreply.github.com>
2024-11-03 21:40:14 +1000

    Categories system

--- forum/views.py
+++ forum/views.py
@@ -2,8 +2,33 @@ from django.shortcuts import render, redirect
 from .forms import TopicForm
 from .models import Topic, Tag
 
+from django.shortcuts import render, redirect
+from .forms import TopicForm
+from .models import Topic, Tag
+
+CATEGORY_COLORS = {
+    'hobby': ('bg-f9bc64', 'Хобби'),
+    'video': ('bg-4436f8', 'Видео'),
+    'art': ('bg-ff755a', 'Искусство'),
+    'games': ('bg-83253f', 'Игры'),
+    'health': ('bg-3ebafa', 'Здоровье'),
+    'entertainment': ('bg-a7cdbd', 'Развлечения'),
+    'qa': ('bg-777da7', 'Q&A'),
+    'society': ('bg-348aa7', 'Общество'),
+    'random': ('bg-5dd39e', 'Случайное'),
+    'tech': ('bg-bce784', 'Технологии'),
+    'science': ('bg-c49bbb', 'Наука'),
+    'animals': ('bg-c6b38e', 'Животные'),
+    'education': ('bg-525252', 'Образование'),
+    'politics': ('bg-368f8b', 'Политика'),
+}
+
 def main_str(request):
     topics = Topic.objects.all()
+    for topic in topics:
+        color, name = CATEGORY_COLORS.get(topic.category, ('bg-default', 'Категория'))
+        topic.category_color = color
+        topic.category_name = name
     return render(request, 'forum/index.html', {'topics': topics})
 
 def create_topic(request):
