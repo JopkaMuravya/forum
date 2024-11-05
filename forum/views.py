a53df2f5b9a380fc3be64608e68f19edabdc0c9f
Nania01 <148682556+Nania01@users.noreply.github.com>
2024-11-06 02:17:17 +1000

    fix

--- forum/views.py
+++ forum/views.py
@@ -114,6 +114,9 @@ def main_str(request, category=None):
     tags = Tag.objects.all()
     topics = Topic.objects.all()
 
+    if request.GET.get('my_topics') == 'true' and request.user.is_authenticated:
+        topics = topics.filter(author=request.user)
+
     if request.GET.get('liked_topics') == 'true' and request.user.is_authenticated:
         topics = topics.filter(likes__user=request.user)
 
