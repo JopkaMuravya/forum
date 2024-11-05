98646d367c0a4cb352e981b7b1775276afa3eed3
empath105 <150247993+empath105@users.noreply.github.com>
2024-11-06 08:34:18 +1000

    New

--- forum/urls.py
+++ forum/urls.py
@@ -8,10 +8,13 @@ urlpatterns = [
     path('category/<str:category>/', views.main_str, name='category_filter'),
     path('create-topic/', views.create_topic, name='create-topic'),
     path('signup/', views.register, name='signup'),
+    path('verify_email/<str:token>/', views.verify_email, name='verify_email'),
     path('signin/', views.user_login, name='signin'),
     path('topic/<int:id>/', views.single_topic, name='single_topic'),
+    path('like/<int:topic_id>/', views.toggle_like, name='toggle_like'),
     path('acaunt/', views.user_account, name='acaunt'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
